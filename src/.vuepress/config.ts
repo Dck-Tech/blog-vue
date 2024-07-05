23d8146c3f8588ba15b522ec3c1b08e1b263b8cb
daupv-innove <admin@dck-tech.com>
2024-07-06 01:48:09 +0900

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -25,7 +25,7 @@ export default defineUserConfig({
     "/zh/": {
       lang: "zh-CN",
       title: "キリト",
-      description: "vuepress-themeキリト-hope 的博客演示",
+      description: "vuepress-theme-hope 的博客演示",
     },
 
   },
